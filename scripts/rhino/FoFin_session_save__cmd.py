23e777faedbfd3de274f8907e2b1cf5e41e0cc66
brgcode <van.mele@arch.ethz.ch>
2021-12-13 22:16:02 +0100

    test commands

---
+++ scripts/rhino/FoFin_session_save__cmd.py
@@ -0,0 +1,18 @@
+from compas_ui.app import App
+# from compas_fofin.ui import FofinApp
+
+
+def RunCommand(interactive=True):
+
+    # make App a singleton
+    # and intialize a session when the singleton is created
+    app = App('FoFin')
+    app.save_session()
+
+
+# ******************************************************************************
+# Run as Script
+# ******************************************************************************
+
+if __name__ == "__main__":
+    RunCommand()
