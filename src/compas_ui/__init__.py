70b90aadc267ea6b013e98fdc239cf5dda9b1ec5
brgcode <van.mele@arch.ethz.ch>
2021-12-13 22:13:05 +0100

    register plugins

--- src/compas_ui/__init__.py
+++ src/compas_ui/__init__.py
@@ -33,3 +33,7 @@ TEMP = os.path.abspath(os.path.join(HOME, "temp"))
 
 
 __all__ = ["HOME", "DATA", "DOCS", "TEMP"]
+__all_plugins__ = [
+    'compas_ui.rhino.install',
+    'compas_ui.rhino.scene'
+]
