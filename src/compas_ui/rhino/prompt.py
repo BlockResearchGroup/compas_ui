944b2e1cfb196bcc6a1e10c14c6c7d64e9daae27
brgcode <van.mele@arch.ethz.ch>
2022-02-04 11:01:15 +0100

    simplify to scene, session, objects

--- src/compas_ui/rhino/prompt.py
+++
@@ -1,7 +0,0 @@
-from compas.plugins import plugin
-import compas_rhino
-
-
-@plugin(category='ui', requires=['Rhino'])
-def get_real(message=None, default=None, minimum=None, maximum=None):
-    return compas_rhino.rs.GetReal(message=message, number=default, minimum=minimum, maximum=maximum)
--- src/compas_ui/view.py
+++
@@ -1,7 +0,0 @@
-from .camera import Camera
-
-
-class View(object):
-
-    def __init__(self, app, camera=None):
-        self.app = app
