5048e6625feb8034ae66e6a644daadd34ef954de
brgcode <van.mele@arch.ethz.ch>
2022-02-11 23:09:27 +0100

    transfer

--- src/compas_ui/session.py
+++ src/compas_ui/session.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import sys
 import os
 import atexit
@@ -14,6 +18,9 @@ def autosave():
     """Automatically save the session to a file by registering this function with `atexit`."""
     session = Session()
     session.save()
+
+
+atexit.register(autosave)
 
 
 class Session(object):
@@ -204,6 +211,3 @@ class Session(object):
 
         """
         pass
-
-
-atexit.register(autosave)
