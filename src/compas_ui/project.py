59f2de758a96946964d63130c19f72e19bf2b388
Licini <leochen1990@gmail.com>
2022-04-28 16:22:26 +0200

    using compas_stream

--- src/compas_ui/project.py
+++ src/compas_ui/project.py
@@ -1,72 +1,60 @@
-from compas_ui.stream import Stream
+from compas_stream.project import Project
+from compas_stream.stream import checkout
+from compas.data import json_dumps, json_loads
+import json
 
-class Project(object):
+
+class Project(Project):
 
     def __init__(self, ui):
-        self._ui = ui
-        self.stream = Stream(self)
+        super().__init__()
+        self.ui = ui
         self.name = "Untitled"
-        self.data = {}
-        self.scenes = {}
-        self.session = {}
 
-    def __getstate__(self):
-        return self.state
-
-    def __setstate__(self, state):
-        self.state = state
-
-    @property
-    def ui(self):
-        if not hasattr(self, '_ui') or self._ui is None:
-            from compas_ui.ui import UI
-            self._ui = UI()
-        return self._ui
-
-    @property
-    def state(self):
-        # gather data stream_states
-        self.data.clear()
+    def update_tracking(self):
         for obj in self.ui.scene.objects:
-            if str(obj.item.guid) not in self.data:
-                if not hasattr(obj.item, 'stream'):
-                    obj.item.stream = Stream(obj.item) # Create stream if not present
-                self.data[str(obj.item.guid)] = obj.item.stream.state
+            data = obj.item
 
-        # gather scene stream_states
-        self.scenes.clear()
-        for scene in [self.ui.scene]:
-            self.scenes[str(scene.guid)] = scene.stream.state
+            def getter():
+                return json.loads(json_dumps(data))
 
-        # Update session data
-        self.session = self.ui.session.data
+            def setter(self, content):
+                self.data = json_loads(json.dumps(content)).data
 
-        state = self.__dict__.copy()
-        del state['_ui']
-        return state
+            self.track(data.guid, data, getter=getter, setter=setter)
 
-    @state.setter
-    def state(self, state):
-        self.__dict__.update(state)
-        self.ui.session.data = self.ui.session
+        self.track(self.ui.scene.guid, self.ui.scene)
+        self.track(self.guid, self)
 
-    def load_from_stream(self):
+    @property
+    def stream_content(self):
+
+        self.update_tracking()
+        content = {"data": {}, "scenes": {}}
+
+        # gather data stream states
+        for obj in self.ui.scene.objects:
+            guid = str(obj.item.guid)
+            content["data"][guid] = self.streams[guid].state
+
+        # gather scene stream states
+        guid = str(self.ui.scene.guid)
+        content["scenes"][guid] = self.streams[guid].state
+
+        return content
+
+    @stream_content.setter
+    def stream_content(self, content):
+
         # Load data
         data = {}
-        for guid, stream_state in self.state['data'].items():
+        for guid, stream_state in content["data"].items():
             # Recreate data item from stream state
-            item = self.ui.proxy.speckle_operation("checkout", stream_state)
-            item.stream = Stream.from_state(item, stream_state)
-            data[guid] = item
+            data[guid] = checkout(**stream_state)
 
         # Load scene
         # TODO: this needs to be adjusted when multi scenes are supported.
-        scene = self.ui.scene
-        for guid, stream_state in self.state['scenes'].items():
-            scene.clear()
-            scene_state = self.ui.proxy.speckle_operation("checkout", stream_state)
-            scene_state['data'] = data
-            scene.state = scene_state
-            scene._guid = guid
-            scene.stream = Stream.from_state(scene, stream_state)
-            scene.update()
+        for guid, stream_state in content["scenes"].items():
+            scene_state = checkout(**stream_state)
+            scene_state["data"] = data
+            self.ui.scene.state = scene_state
