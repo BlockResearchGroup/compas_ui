dd242ccfd4d72b931df7c00c7f23a0a5cd43c6bd
brgcode <tom.v.mele@gmail.com>
2022-08-29 20:19:26 +0200

    handle empty edge color dicts

--- src/compas_ui/rhino/artists/meshartist.py
+++ src/compas_ui/rhino/artists/meshartist.py
@@ -4,12 +4,15 @@ from compas_rhino.artists import MeshArtist
 class MeshArtist(MeshArtist):
     @property
     def state(self):
+        _edge_color = self._edge_color or {}
         return {
             "default_vertexcolor": self.default_vertexcolor,
             "default_edgecolor": self.default_edgecolor,
             "default_facecolor": self.default_facecolor,
             "_vertex_color": self._vertex_color,
-            "_edge_color": self._edge_color,
+            "_edge_color": {
+                "{},{}".format(*edge): self._edge_color[edge] for edge in _edge_color
+            },
             "_face_color": self._face_color,
             "vertices": self.vertices,
             "edges": self.edges,
@@ -22,7 +25,10 @@ class MeshArtist(MeshArtist):
         self.default_edgecolor = state["default_edgecolor"]
         self.default_facecolor = state["default_facecolor"]
         self._vertex_color = state["_vertex_color"]
-        self._edge_color = state["_edge_color"]
+        self._edge_color = {
+            (int(edge[0]), int(edge[1])): state["_edge_color"][edge]
+            for edge in state["_edge_color"]
+        }
         self._face_color = state["_face_color"]
         self.vertices = state["vertices"]
         self.edges = state["edges"]
