18dc8c7c940b360026bc04f345644c7acdb536cb
brgcode <tom.v.mele@gmail.com>
2022-08-29 14:24:37 +0200

    re-add record

--- src/compas_ui/rhino/ui/COMPAS/dev/COMPAS_scene_objects_cmd.py
+++ src/compas_ui/rhino/ui/COMPAS/dev/COMPAS_scene_objects_cmd.py
@@ -1,6 +1,5 @@
 from compas_ui.ui import UI
 
-# TODO: turn off recording for now
 # TODO: add namespace per plugin
 # TODO: root objects under namespaces
 # TODO: provide COMPAS as default namespace
@@ -15,7 +14,7 @@ def RunCommand(is_interactive):
 
     ui = UI()
     ui.scene_objects()
-    # ui.record()
+    ui.record()
 
 
 if __name__ == "__main__":
