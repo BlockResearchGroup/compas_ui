0298c613570d11759ece4aafd6e87fd02ece3314
brgcode <tom.v.mele@gmail.com>
2022-08-30 21:07:59 +0200

    add state to curve artist

--- src/compas_ui/rhino/artists/__init__.py
+++ src/compas_ui/rhino/artists/__init__.py
@@ -6,15 +6,18 @@ from compas.plugins import plugin
 from compas.artists import Artist
 
 from compas.geometry import Line
+from compas.geometry import Curve
 from compas.datastructures import Mesh
 
 from .lineartist import LineArtist
+from .curveartist import CurveArtist
 from .meshartist import MeshArtist
 
 
 @plugin(category="factories", requires=["Rhino"])
 def register_artists():
     Artist.register(Line, LineArtist, context="Rhino")
+    Artist.register(Curve, CurveArtist, context="Rhino")
     Artist.register(Mesh, MeshArtist, context="Rhino")
 
     print("UI Rhino Artists registered.")
@@ -22,5 +25,6 @@ def register_artists():
 
 __all__ = [
     "LineArtist",
+    "CurveArtist",
     "MeshArtist",
 ]
