64559c34e4304c4599165ed9985284da7714dfaa
brgcode <tom.v.mele@gmail.com>
2022-08-30 21:07:18 +0200

    black

--- src/compas_ui/rhino/artists/lineartist.py
+++ src/compas_ui/rhino/artists/lineartist.py
@@ -1,3 +1,7 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
 from compas_rhino.artists import LineArtist
 
 
@@ -11,5 +15,5 @@ class LineArtist(LineArtist):
 
     @state.setter
     def state(self, state):
-        self.default_color = state['default_color']
-        self.color = state['color']
+        self.default_color = state["default_color"]
+        self.color = state["color"]
