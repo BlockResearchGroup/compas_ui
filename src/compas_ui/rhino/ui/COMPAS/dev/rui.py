4a46befefee7340ec91bbc48251fb4397c0437b7
brgcode <tom.v.mele@gmail.com>
2022-09-05 22:47:51 +0200

    split up config files

--- src/compas_ui/rhino/ui/COMPAS/dev/rui.py
+++ src/compas_ui/rhino/ui/COMPAS/dev/rui.py
@@ -2,9 +2,9 @@ import os
 from compas_ui.rhino.rui import Rui
 
 HERE = os.path.dirname(__file__)
-CONFIGPATH = os.path.join(HERE, "config.json")
+UIPATH = os.path.join(HERE, "ui.json")
 RUIPATH = os.path.join(HERE, "COMPAS.rui")
 
-rui = Rui.from_json(CONFIGPATH, RUIPATH)
+rui = Rui.from_json(UIPATH, RUIPATH)
 
 rui.write()
