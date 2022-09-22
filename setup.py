e1951dac2f636d46d525e0fbd7d2dd9a8903672b
Li <leochen1990@gmail.com>
2022-09-22 13:06:52 +0200

    Bump version to 0.5.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {}
 
 setup(
     name="compas_ui",
-    version="0.5.0",
+    version="0.5.1",
     description="COMPAS UI provides basic functionality to build a user interface for a COMPAS app.",
     long_description=long_description,
     long_description_content_type="text/markdown",
