0dfbe556ee93b3b5364eddc78032a4599aaea369
brgcode <tom.v.mele@gmail.com>
2022-11-09 23:09:58 +0100

    clean up the installation

--- src/compas_ui/rhino/install.py
+++ src/compas_ui/rhino/install.py
@@ -1,20 +1,35 @@
 import os
 import sys
-import json
+
 from shutil import copyfile
 from subprocess import call
 
 import compas
 from compas.plugins import plugin
 import compas_rhino
-from compas_rhino.install import install as install_packages
-from compas_rhino.install import _filter_installable_packages
+
 from compas_rhino.install_plugin import install_plugin
 import pkg_resources
 from pkg_resources import DistributionNotFound, VersionConflict
 
 
 HERE = os.path.dirname(__file__)
+
+
+def get_version_from_args():
+    args = compas_rhino.INSTALLATION_ARGUMENTS
+    return compas_rhino._check_rhino_version(args.version)
+
+
+@plugin(category="install", tryfirst=True)
+def after_rhino_install(installed_packages):
+    if "compas_cloud" not in installed_packages:
+        return []
+
+    version = get_version_from_args()
+    install(version)
+
+    return [("compas_ui", "COMPAS UI installed", True)]
 
 
 @plugin(category="install", tryfirst=True)
@@ -62,21 +77,6 @@ def check_folders(plugin_name, version="7.0"):
         print("User has write access to the Rhino scripts folder")
         print("PASSED.\n")
 
-        print("Checking that the Rhino PythonPlugins folder exists")
-        python_plugins_path = compas_rhino._get_rhino_pythonplugins_path(version)
-        if not os.path.isdir(python_plugins_path):
-            raise Exception("Cannot find the PythonPlugins folder at: {}".format(python_plugins_path))
-        print(python_plugins_path, "Exists")
-        print("PASSED.\n")
-
-        print("Checking that user has write access to the Rhino PythonPlugins folder")
-        if not os.access(python_plugins_path, os.W_OK):
-            raise Exception(
-                "User does not have write access to the Rhino PythonPlugins folder at: {}".format(python_plugins_path)
-            )
-        print("User has write access to the Rhino PythonPlugins folder")
-        print("PASSED.\n")
-
         print("\n All checks passed.")
         return True
 
@@ -119,20 +119,7 @@ def install(version="7.0"):
 
     plugin_path = os.path.abspath(plugin_path)
     plugin_dev = os.path.join(plugin_path, "dev")
-    plugin_config = os.path.join(plugin_dev, "config.json")
 
-    with open(plugin_config, "r") as f:
-        config = json.load(f)
-
-    packages = []
-    packages = _filter_installable_packages(version, packages)
-
-    if "packages" in config:
-        for name in config["packages"]:
-            if name not in packages:
-                packages.append(name)
-
-    install_packages(version=version, packages=packages)
     install_plugin(plugin_path)
 
     if compas.WINDOWS:
@@ -147,30 +134,7 @@ def install(version="7.0"):
         )
 
 
-def main(plugin_name, version):
-    print("=" * 20, "Checking Folders", "=" * 20)
-    if not check_folders(plugin_name, version):
-        return
-
-    # print("="*20, "Checking Dependencies", "="*20)
-    # if not check_dependencies():
-    #     return
-
-    print("=" * 20, "Running Installation", "=" * 20)
-    install(version)
-
-    print("=" * 20, "Installation Completed", "=" * 20)
-
-
 if __name__ == "__main__":
-    import argparse
 
-    parser = argparse.ArgumentParser(description="COMPAS UI installer command line utility.")
-    parser.add_argument(
-        "--version",
-        default="7.0",
-        choices=["6.0", "7.0"],
-        help="Version of Rhino.",
-    )
-    args = parser.parse_args()
-    main("COMPAS", args.version)
+    print("This installation procedure is deprecated.")
+    print("Use `python -m compas_rhino.install` instead.")
