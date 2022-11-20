da49a6d358cecc5931581ab77f54d20cc8ff3499
brgcode <tom.v.mele@gmail.com>
2022-11-20 16:19:50 +0100

    fix bugs in mesh artist state

--- src/compas_ui/artists/meshartist.py
+++ src/compas_ui/artists/meshartist.py
@@ -10,7 +10,7 @@ class MeshArtist(MeshArtist):
             "default_edgecolor": self.default_edgecolor,
             "default_facecolor": self.default_facecolor,
             "vertex_color": self._vertex_color,
-            "edge_color": {"{},{}".format(*edge): self._edge_color[edge] for edge in _edge_color},
+            "edge_color": {"{},{}".format(*edge): _edge_color[edge] for edge in _edge_color},
             "face_color": self._face_color,
             "vertices": self.vertices,
             "edges": self.edges,
@@ -19,11 +19,16 @@ class MeshArtist(MeshArtist):
 
     @state.setter
     def state(self, state):
+        edge_color = {}
+        for key in state["edge_color"]:
+            edge = key.split(",")
+            edge = int(edge[0]), int(edge[1])
+            edge_color[edge] = state["edge_color"][key]
         self.default_vertexcolor = state["default_vertexcolor"]
         self.default_edgecolor = state["default_edgecolor"]
         self.default_facecolor = state["default_facecolor"]
         self._vertex_color = state["vertex_color"]
-        self._edge_color = {(int(edge[0]), int(edge[1])): state["edge_color"][edge] for edge in state["edge_color"]}
+        self._edge_color = edge_color
         self._face_color = state["face_color"]
         self.vertices = state["vertices"]
         self.edges = state["edges"]
